eafc5dd6fede47e90e272baaec443ba24b7eb1c6
AntonyBil <beletskiy.ant@gmail.com>
2023-09-18 16:35:47 +0300

    List navigation link and pass data betvin VC

--- ToDoList/DetailView.swift
+++ ToDoList/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     @Environment(\.dismiss) private var dismiss
+    var passedValue: String
     
     var body: some View {
         VStack {
@@ -17,7 +18,7 @@ struct DetailView: View {
                 .scaledToFit()
                 .foregroundColor(.orange)
             
-            Text("You Are a Swifty Legend")
+            Text("You Are a Swifty Legend!\nAnd you passed over the value\(passedValue)")
                 .font(.largeTitle)
                 .multilineTextAlignment(.center)
                 .foregroundColor(.orange)
@@ -30,12 +31,12 @@ struct DetailView: View {
             .buttonStyle(.borderedProminent)
         }
         .padding()
-        .navigationBarBackButtonHidden()
+//        .navigationBarBackButtonHidden()
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(passedValue: "")
     }
 }
--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -8,19 +8,51 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    
+    var toDos = ["Learn Swift", "Build Apps", "Chenge the World", "Bring the Awesome", "Taca a Vacation" ]
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                NavigationLink {
-                    DetailView()
-                } label: {
-                    Image(systemName: "eye")
-                    Text("Show the New View")
+            List {
+                ForEach(toDos, id: \.self) { toDo in
+                    NavigationLink {
+                        DetailView(passedValue: toDo)
+                    } label: {
+                        Text(toDo)
+                    }
+
+                   
                 }
-                .buttonStyle(.borderedProminent)
+                
+
+                
+//                Section {
+//                    NavigationLink {
+//                        DetailView()
+//                    } label: {
+//                        Text("Winter")
+//                    }
+//
+//                    NavigationLink {
+//                        DetailView()
+//                    } label: {
+//                        Text("Summer")
+//                    }
+//                } header: {
+//                    Text("Breaks")
+//                }
+//
+//                Section {
+//                    Text("Spring")
+//                    Text("Fall")
+//                }header: {
+//                    Text("Semesters")
+//                }
             }
+            .navigationTitle("To Do List")
+            .navigationBarTitleDisplayMode(.automatic)
+            .listStyle(.plain)
         }
-        
         .padding()
     }
 }
